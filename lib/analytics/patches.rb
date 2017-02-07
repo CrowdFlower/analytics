2429c4a0da8ece66f5d090ff936986b4963d4b0b
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2017-02-07 14:46:01 -0800

    Add context to track_and_identify

--- lib/analytics/patches.rb
+++ lib/analytics/patches.rb
@@ -1,8 +1,8 @@
 module AnalyticsRuby
-  def self.track_and_identify(event, properties, user)
-    identify(:user_id => user.id, :traits => user.identity_payload)
+  def self.track_and_identify(event, properties, user, context={})
+    identify(:user_id => user.id, :traits => user.identity_payload, :context => context)
     #Hubspot and others need an email address in the event...
     properties = {:email => user.email}.merge(properties)
-    track(:user_id => user.id, :event => event, :properties => properties)
+    track(:user_id => user.id, :event => event, :properties => properties, :context => context)
   end
 end
